b273a55a7e627c33e2145d224e076d9de502b55f
Harshit Budhlakoti <harshitbudhlakoti00@gmail.com>
2022-10-06 16:01:35 +0530

    updated as per suggestions in pr?

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -6,12 +6,11 @@ export default class ProductsController {
   public async index(ctx: HttpContextContract) {
     const query = ctx.request.qs()
     const page: number = query.page || 1;
-    let sortedProducts = products.filter((item) => {
-      const title = item.category;
-      const search: string = query.search || '';
-      return title.toLowerCase().indexOf(search.toLowerCase()) != -1;
+    const search: string = query.search || '';
+    let searchedProducts = products.filter((item) => {
+      return item.category.toLowerCase().includes(search.toLowerCase())
     });
-    const finalProducts = sortedProducts.filter((_, index) => index >= (page-1)*20 && index < (page * 20));
-    return finalProducts
+    return searchedProducts.slice((page - 1) * 20, (page * 20))
+
   }
 }
