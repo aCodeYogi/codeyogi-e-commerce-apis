af070eb3c8fd0b84241cac59036859a64dfae935
Harshit Budhlakoti <harshitbudhlakoti00@gmail.com>
2022-10-12 12:39:08 +0530

    product api updated

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -2,27 +2,25 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Product from 'App/Models/Product'
 
 export default class ProductsController {
-    public async index({ request, response }: HttpContextContract) {
-        const { sortBy, sortType, page, search } = request.qs() as {
-            sortBy?: string
-            sortType?: string
-            page?: string
-            search?: string
-        }
-        if (sortType !== 'asc' && sortType !== 'desc' && sortType !== undefined) {
-            return response.badRequest('search is invalid')
-        }
-        let productsQueryBuilder = Product.query()
-        if (search) {
-            productsQueryBuilder = productsQueryBuilder.whereRaw('LOWER(title) like ?', [
-                `%${search.toLowerCase()}%`,
-            ])
-        }
-        if (sortBy) {
-            productsQueryBuilder = productsQueryBuilder.orderBy(sortBy, sortType ? sortType : 'asc')
-        }
-        const products = productsQueryBuilder.paginate(page ? +page : 1, 20)
-
-        return products
+  public async index({ request, response }: HttpContextContract) {
+    const { sortBy, sortType, page, search } = request.qs() as {
+      sortBy?: string
+      sortType?: string
+      page?: string
+      search?: string
     }
+    if (sortType !== 'asc' && sortType !== 'desc' && sortType !== undefined) {
+      return response.badRequest('search is invalid')
+    }
+    let productsQueryBuilder = Product.query()
+    if (search) {
+      productsQueryBuilder = productsQueryBuilder.whereRaw('LOWER(title) like ?', [`%${search.toLowerCase()}%`])
+    }
+    if (sortBy) {
+      productsQueryBuilder = productsQueryBuilder.orderBy(sortBy, sortType ? sortType : 'asc')
+    }
+    const products = productsQueryBuilder.paginate(page ? +page : 1, 20)
+
+    return products
+  }
 }
